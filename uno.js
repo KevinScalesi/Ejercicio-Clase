2d33bc7189cda4469a76c1d0033ed4c22a138586
KevinScalesi <kevin_scalesi@yahoo.com.ar>
2020-09-02 19:34:05 -0300

    otro cambio

--- uno.js
+++ uno.js
@@ -3,7 +3,7 @@ Modificado
 Un par de líneas de texto que tengan
 sentido porque va hacer más sencillo el próximo paso.
 
-
+//"esto es una prueba de conflicto"
 
 let alumno = "Soy kevin, de mendoza. tengo 30 años y apasionado por la tecnología"
 

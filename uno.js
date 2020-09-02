65cc823b9e6104b8c47a2bd67d4063f0753f5123
SoyDani <elidaniypri@gmail.com>
2020-09-02 19:34:49 -0300

    cambio para crear conflicto

--- uno.js
+++ uno.js
@@ -3,7 +3,7 @@ Modificado
 Un par de líneas de texto que tengan
 sentido porque va hacer más sencillo el próximo paso.
 
-
+let linea = 6
 
 let alumno = "Soy kevin, de mendoza. tengo 30 años y apasionado por la tecnología"
 
